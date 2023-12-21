acf231e3dba90f3411a4979a8d250b95becca9c7
OsneiverC <Osneiver2020@gmail.com>
2023-12-21 15:50:00 -0500

    metas actualizados -overflow activado

--- src/js/vendor/main.js
+++ src/js/vendor/main.js
@@ -8,11 +8,12 @@ const menu = document.querySelector('nav')
 cerrar.addEventListener('click', function(){
     menu.classList.remove('absolute')
     menu.classList.add('hidden');
+    document.body.classList.remove('overflow-hidden')
 })
 
 abrir.addEventListener('click', function(){
     menu.classList.remove('hidden')
     menu.classList.add('absolute');
+    document.body.classList.add('overflow-hidden')
 })
-
 
