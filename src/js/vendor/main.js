9b9ff8e4075d660a726a84c1e98e16794b4edbcc
OsneiverC <Osneiver2020@gmail.com>
2023-12-22 17:11:46 -0500

    nuevos apartados

--- src/js/vendor/main.js
+++ src/js/vendor/main.js
@@ -4,7 +4,6 @@ const menu = document.querySelector('nav')
 
 
 
-
 cerrar.addEventListener('click', function(){
     menu.classList.remove('absolute')
     menu.classList.add('hidden');
@@ -17,3 +16,21 @@ abrir.addEventListener('click', function(){
     document.body.classList.add('overflow-hidden')
 })
 
+function up () {
+    const scroll = document.documentElement.scrollTop
+    if (scroll > 0){
+        window.requestAnimationFrame(up)
+        window.scrollTo (0, scroll - (scroll / 5) ) 
+    }
+    
+}
+document.getElementById('up').addEventListener('click', up)
+
+window.onscroll = function(){
+    const scroll = document.documentElement.scrollTop
+    if (scroll > 500){
+        document.getElementById('up').style.transform = 'scale(1)'
+    }else if (scroll < 500){
+        document.getElementById('up').style.transform = 'scale(0)'
+    }
+}
